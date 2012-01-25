e3f52bce8e52fb82bc823b8f6a3434122b6674d1
VKorostelev <vkorostelev@webzavod.ru>
2012-01-25 15:57:41 +0400

    using new ability to autoincrement fields

--- dentist-nb/src/ru/terra/dentist/dto/Patient.java
+++ dentist-nb/src/ru/terra/dentist/dto/Patient.java
@@ -18,63 +18,63 @@ public class Patient
     private String patName;
     private String patMName;
 
-    @PropGetter(id = Constants.ID_PROP_ID)
+    @PropGetter(id = Constants.ID_PROP_ID, autoincrement = "true")
     public Integer getPatId()
     {
-	return patId;
+        return patId;
     }
 
     @PropSetter(id = Constants.ID_PROP_ID)
     public void setPatId(Integer patId)
     {
-	this.patId = patId;
+        this.patId = patId;
     }
 
     @PropGetter(id = Constants.PAT_MIDNAME_PROP_ID)
     public String getPatMName()
     {
-	return patMName;
+        return patMName;
     }
 
     @PropSetter(id = Constants.PAT_MIDNAME_PROP_ID)
     public void setPatMName(String patMName)
     {
-	this.patMName = patMName;
+        this.patMName = patMName;
     }
 
     @PropGetter(id = Constants.PAT_NAME_PROP_ID)
     public String getPatName()
     {
-	return patName;
+        return patName;
     }
 
     @PropSetter(id = Constants.PAT_NAME_PROP_ID)
     public void setPatName(String patName)
     {
-	this.patName = patName;
+        this.patName = patName;
     }
 
     @PropGetter(id = Constants.PAT_NUM_PROP_ID)
     public Integer getPatNum()
     {
-	return patNum;
+        return patNum;
     }
 
     @PropSetter(id = Constants.PAT_NUM_PROP_ID)
     public void setPatNum(Integer patNum)
     {
-	this.patNum = patNum;
+        this.patNum = patNum;
     }
 
     @PropGetter(id = Constants.PAT_SURNAME_PROP_ID)
     public String getPatSName()
     {
-	return patSName;
+        return patSName;
     }
 
     @PropSetter(id = Constants.PAT_SURNAME_PROP_ID)
     public void setPatSName(String patSName)
     {
-	this.patSName = patSName;
+        this.patSName = patSName;
     }
 }
