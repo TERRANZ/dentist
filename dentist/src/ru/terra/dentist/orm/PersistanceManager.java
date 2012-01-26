6c762d0b93f141ff2432442e0295d5a6b629cadc
Terranz <vterranz@gmail.com>
2012-01-26 22:42:44 +0400

    Revert "lost files"
    This reverts commit be78105671cd151882dbfe9fcd4cbe15c2b7f27e.

---
+++ dentist/src/ru/terra/dentist/orm/PersistanceManager.java
@@ -0,0 +1,66 @@
+package ru.terra.dentist.orm;
+
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author terranz
+ */
+public abstract class PersistanceManager
+{
+
+    protected Session session = HibernateUtil.getSessionFactory().openSession();
+
+    public List<Object> load(Class entity)
+    {
+        Criteria c = session.createCriteria(entity);
+        return c.list();
+    }
+
+    public void delete(Object o)
+    {
+        try
+        {
+            session.beginTransaction();
+            session.delete(o);
+            session.getTransaction().commit();
+        }
+        catch (HibernateException he)
+        {
+            he.printStackTrace();
+        }
+    }
+
+    public void update(Object o)
+    {
+        try
+        {
+            session.beginTransaction();
+            session.update(o);
+            session.getTransaction().commit();
+        }
+        catch (HibernateException he)
+        {
+            he.printStackTrace();
+        }
+    }
+
+    public void saveNew(Object o)
+    {
+        try
+        {
+            session.beginTransaction();
+            session.save(o);
+            session.getTransaction().commit();
+        }
+        catch (HibernateException he)
+        {
+            he.printStackTrace();
+        }
+    }
+
+    public abstract Object findById(Integer id);   
+}
